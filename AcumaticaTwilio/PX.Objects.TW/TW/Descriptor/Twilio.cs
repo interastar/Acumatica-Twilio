c361fbb96935dbb29fbfd1bc8fab94a91e15e0ed
Marco Villaseñor <marco@interastar.com>
2018-01-24 22:13:11 -0600

    Update Twilio to v5 Fix dll references Rename error message

--- AcumaticaTwilio/PX.Objects.TW/TW/Descriptor/Twilio.cs
+++ AcumaticaTwilio/PX.Objects.TW/TW/Descriptor/Twilio.cs
@@ -3,13 +3,15 @@ using System.Collections.Specialized;
 using System.Linq;
 using PX.Data;
 using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace PX.Objects.TW
 {
     public static class NameValueCollectionExtensions
     {
         /// <summary>
-        /// NameValueCollection extension that transforms the collection into a string with Uri encoded values 
+        /// NameValueCollection extension that transforms the collection into a string with Uri encoded values
         /// for an HTTP Request
         /// </summary>
         /// <param name="collection"></param>
@@ -27,15 +29,24 @@ namespace PX.Objects.TW
     public class TwilioNotification
     {
         private static string TwimletBase = "http://twimlets.com/message";
-        private TwilioRestClient _client;
 
         public string Origin { get; set; }
 
+        /// <summary>
+        /// Initializes the Twilio client with credentials
+        /// </summary>
+        /// <param name="sid">Your Account SID from twilio.com/console</param>
+        /// <param name="token">Your Auth Token from twilio.com/console</param>
         public TwilioNotification(string sid, string token)
         {
-            _client = new TwilioRestClient(sid, token);
+            TwilioClient.Init(sid, token);
         }
 
+        /// <summary>
+        /// Builds Twimlet URL to the message twimlet to generate TwiML for a voice message
+        /// </summary>
+        /// <param name="messages">Text to say</param>
+        /// <returns></returns>
         public static string MessageUrl(params string[] messages)
         {
             var messageCollection = new NameValueCollection();
@@ -48,26 +59,44 @@ namespace PX.Objects.TW
             return TwimletBase + messageCollection.ToQueryString();
         }
 
+        /// <summary>
+        /// Sends an SMS message to the specified number
+        /// </summary>
+        /// <param name="number">Destination number</param>
+        /// <param name="message">Message text</param>
         public void SendSMS(string number, string message)
         {
-            var msg = _client.SendMessage(Origin, number, message);
-
-            if (msg.RestException != null)
+            try
             {
-                throw new PXException(msg.RestException.Message);
+                var msg = MessageResource.Create(
+                    to: new PhoneNumber(number),
+                    from: new PhoneNumber(Origin),
+                    body: message);
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
 
+        /// <summary>
+        /// Makes a call to specified number and says the message
+        /// </summary>
+        /// <param name="number">Destination number</param>
+        /// <param name="message">Message text</param>
         public void SendCall(string number, string message)
         {
-            var call = _client.InitiateOutboundCall(Origin, number, MessageUrl(message));
-
-            if (call.RestException != null)
+            try
             {
-                throw new PXException(call.RestException.Message);
+                var msg = CallResource.Create(
+                to: new PhoneNumber(number),
+                from: new PhoneNumber(Origin),
+                url: new Uri(MessageUrl(message)));
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
-
     }
-
 }
